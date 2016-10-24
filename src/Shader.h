15b721609703f48f897f129ad3b9da71463849a2
AgenoDrei <s.mueller1995@gmail.com>
2016-10-24 20:52:36 +0200

    Setup window handling for glut setup shader class and camera class setup keyboard and mouse input setup basic opengl for triagnle rendering

---
+++ src/Shader.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <iostream>
+
+#include <GL/glew.h>
+
+class Shader
+{
+public:
+    GLuint Program;
+    // Constructor generates the shader on the fly
+    Shader(const GLchar* vertexPath, const GLchar* fragmentPath);
+    // Uses the current shader
+    void Use();
+};
---
+++ src/WindowHandler.h
@@ -0,0 +1,45 @@
+//
+// Created by simon on 24.10.16.
+//
+
+#pragma once
+#define GLEW_STATIC
+#include <GL/glew.h>
+#include <GL/freeglut.h>
+#include <iostream>
+#include "Camera.h"
+
+void debugOutput(unsigned int source, unsigned int type, unsigned int id, unsigned int severity, int length, const char* message, const void* userParam);
+void onShutdown();
+void onResize(int nw, int nh);
+void onKeyDown(unsigned char key, int x, int y);
+void onKeyUp(unsigned char key, int x, int y);
+void onMouse(int x, int y);
+
+
+class WindowHandler {
+public:
+    WindowHandler(unsigned int windowWidth, unsigned int windowHeight);
+    ~WindowHandler();
+    void initWindow(int argc, char* argv[], void (*init)(),void (*mainLoop)());
+    void setCamera(Camera* camera);
+    void resize(int newWidth, int newHeight);
+    void processKeyboard(bool pressed, unsigned char key, int x, int y);
+    void processMouse(int x, int y);
+
+    static WindowHandler* instance;
+    bool getKey(int key);
+    double getDeltaTime();
+    float getWidth(), getHeight();
+    void calculateFPS();
+
+private:
+    unsigned int width, height;
+    Camera* camera;
+    bool keys[1024];
+    long currentTime, lastTime, deltaTime;
+    bool firstMouse = true;
+    int lastX, lastY;
+};
+
+
