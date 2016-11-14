3d1686532bbcd5750d460896c903fe9a7ec90bf9
Nils Schlemminger <nils@schlemminger.org>
2016-11-14 09:34:35 +0100

    Implement basic marching cube polygonise.

--- src/GridCell.cpp
+++ src/GridCell.cpp
@@ -1,11 +1,12 @@
 #include "GridCell.h"
 
-GridCell::GridCell(const glm::vec3 &position, float value) :
-        position(position),
-        value(value) {
+GridCell::GridCell(const glm::vec3 &position, Particle* value[8]) :
+        position(position) {
+    for(auto i = 0; i < 8; i++) {
+        this->value[i] = value[i];
+    }
 }
 
 GridCell::GridCell() {
     position = glm::vec3(0.0f, 0.0f, 0.0f);
-    value = 0.0f;
 }
